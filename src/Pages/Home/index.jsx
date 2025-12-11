4948833a0d2b56dcc45ced247b394bbb4315eeb4
guiabraao <22201483@aluno.cotemig.com.br>
2025-12-11 17:31:11 -0300

    url

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,9 +7,11 @@ import CarrosselLancamentos from '../../Components/CarrosselLancamentos'
 import Carrossel1 from '../../assets/1.png'
 import Carrossel2 from '../../assets/2.png'
 import Carrossel3 from '../../assets/3.png'
+import CarrosselMobile1 from '../../assets/mobile1.png'
+import CarrosselMobile2 from '../../assets/mobile2.png'
+import CarrosselMobile3 from '../../assets/mobile3.png'
 import Banner1 from '../../assets/bannerChuteiras.png'
 import Banner2 from '../../assets/bannerCamisas.png'
-import Test from '../../assets/test.png'
 import ArrowRight from '../../assets/arrowright.svg'
 import ArrowLeft from '../../assets/arrowleft.svg'
 import { Link } from 'react-router-dom'
@@ -32,6 +34,12 @@ export default function Home() {
         { id: 3, image: Carrossel3 }
     ]
 
+    const bannerCarrosselMobile = [
+        { id: 1, image: CarrosselMobile1 },
+        { id: 2, image: CarrosselMobile2 },
+        { id: 3, image: CarrosselMobile3 }
+    ]
+
     const [currentCarrossel, setCurrentCarrossel] = useState(0);
     const [direction, setDirection] = useState(0);
     const timeoutRef = useRef(null)
@@ -42,6 +50,15 @@ export default function Home() {
             newDirection === 1
                 ? (prev + 1) % bannerCarrossel.length
                 : (prev - 1 + bannerCarrossel.length) % bannerCarrossel.length
+        );
+    };
+
+    const paginateMobile = (newDirection) => {
+        setDirection(newDirection);
+        setCurrentCarrossel((prev) =>
+            newDirection === 1
+                ? (prev + 1) % bannerCarrosselMobile.length
+                : (prev - 1 + bannerCarrosselMobile.length) % bannerCarrosselMobile.length
         );
     };
 
@@ -106,6 +123,43 @@ export default function Home() {
                             <div className="boxBtnsCarrossel">
                                 <div className="boxBtnsCarrossel">
                                     {bannerCarrossel.map((_, index) => (
+                                        <div
+                                            key={index}
+                                            className={`btnBox ${index === currentCarrossel ? "active" : ""}`}
+                                            onClick={() => goToSlide(index)}
+                                        ></div>
+                                    ))}
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="carrosselMobile">
+                            <AnimatePresence mode='wait' custom={direction}>
+                                <motion.img
+                                    key={bannerCarrosselMobile[currentCarrossel].id}
+                                    src={bannerCarrosselMobile[currentCarrossel].image}
+                                    className='boxCarrosselHome'
+                                    custom={direction}
+                                    variants={variantsInertia}
+                                    initial='enter'
+                                    animate='center'
+                                    exit="exit"
+                                    drag="x"
+                                    dragConstraints={{ left: 0, right: 0 }}
+                                    onDragEnd={(e, { offset, velocity }) => {
+                                        const swipe = Math.abs(offset.x) * velocity.x;
+                                        if (swipe < -1000) {
+                                            paginate(1);
+                                        } else if (swipe > 1000) {
+                                            paginate(-1);
+                                        }
+                                    }}
+                                />
+                            </AnimatePresence>
+                            <div className="boxBtnsCarrossel">
+                                <div className="boxBtnsCarrossel">
+                                    {bannerCarrosselMobile.map((_, index) => (
                                         <div
                                             key={index}
                                             className={`btnBox ${index === currentCarrossel ? "active" : ""}`}
